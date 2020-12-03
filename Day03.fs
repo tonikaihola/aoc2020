6f7861261c3a94044029fac7e36f3db24e0b80ff
Toni Kaihola <tonikaihola tonikaihola@users.noreply.github.com>
2020-12-03 18:29:44 +0200

    Day 3 part 2

--- Day03.fs
+++ Day03.fs
@@ -7,7 +7,7 @@ open Utils
 
 module Day03 =
 
-    let rec ride slopeRows slopeCols (lines:string array) (row:int) (col:int) (count:int) =
+    let rec ride (lines:string array) (row:int) (col:int) (count:int) slopeRows slopeCols =
         if row >= lines.Length
         then count
         else 
@@ -22,9 +22,19 @@ module Day03 =
                 if col + slopeCols >= line.Length
                 then col + slopeCols - line.Length
                 else col + slopeCols
-            ride slopeRows slopeCols lines row col count
+            ride lines row col count slopeRows slopeCols 
 
     let run () =
         let lines = System.IO.File.ReadAllLines $"{__SOURCE_DIRECTORY__}/input/day03.txt"
-        let rows, cols = 1, 3
-        ride rows cols lines 0 0 0 |> string
+        let slopes =
+            [ 
+                1, 1
+                1, 3
+                1, 5
+                1, 7
+                2, 1
+            ]
+        let ride = ride lines 0 0 0
+        let slopeTrees = slopes |> List.map (fun (rows,cols) -> ride rows cols)
+        let total = slopeTrees |> List.reduce (*)
+        string total
