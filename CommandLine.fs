805a1f01cea9a1105779e3352a3d621380d5bfb2
Toni Kaihola <tonikaihola tonikaihola@users.noreply.github.com>
2020-12-04 21:44:12 +0200

    Add command line parsing with Argu

---
+++ CommandLine.fs
@@ -0,0 +1,18 @@
+﻿namespace Aoc2020
+
+open Argu
+
+module CommandLine =
+
+    [<CliPrefix(CliPrefix.DoubleDash)>]
+    type AocArgs =
+        | [<AltCommandLine("-d")>] Day of int
+
+        interface IArgParserTemplate with
+            member this.Usage =
+                match this with
+                | Day _ -> "specify day number"
+
+    let parse argv = 
+        let parser = ArgumentParser.Create<AocArgs>(programName = "aoc2020.exe")
+        parser.ParseCommandLine(inputs = argv, raiseOnUsage = true)
--- Program.fs
+++ Program.fs
@@ -1,26 +1,28 @@
 namespace Aoc2020
 
 open System
+open CommandLine
 
 module Main =
 
     [<EntryPoint>]
     let main argv =
-        let result = 
-            if argv.Length = 0 
-            then 
-                "enter day number"
-            else
-                match argv.[0] with
-                | "1" -> Day01.run () |> sprintf "Product of 2020: %s"
-                | "2" -> Day02.run () |> sprintf "Valid pwds: %A"
-                | "3" -> Day03.run () |> sprintf "Tree count: %s"
-                | "4" -> 
+        try
+            let cliArgs = CommandLine.parse argv
+            let result = 
+                match cliArgs.GetResult Day with 
+                | 1 -> Day01.run () |> sprintf "Product of 2020: %s"
+                | 2 -> Day02.run () |> sprintf "Valid pwds: %A"
+                | 3 -> Day03.run () |> sprintf "Tree count: %s"
+                | 4 -> 
                     let passports,validCount,invalidCount = Day04.run ()
                     let testResult = Day04Tests.test ()
                     let newline = Environment.NewLine
                     sprintf "Passports: %A%s// Valid: %i , Invalid: %i %s// Test valid/invalid OK: %A" 
                             passports newline validCount invalidCount newline testResult
-                | _ -> "enter day number"
-        printfn "%s" result
+                | _ -> "unknown day"
+            printfn "%s" result
+        with e ->
+            printfn "%s" e.Message
+        
         0 // return an integer exit code
