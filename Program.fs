238b8c7b201ba9a79f1a8e98acfd148eed754699
Toni Kaihola <tonikaihola tonikaihola@users.noreply.github.com>
2020-12-04 21:47:20 +0200

    Cleanup

--- Program.fs
+++ Program.fs
@@ -5,22 +5,25 @@ open CommandLine
 
 module Main =
 
+    let run day = 
+        match day with 
+        | 1 -> Day01.run () |> sprintf "Product of 2020: %s"
+        | 2 -> Day02.run () |> sprintf "Valid pwds: %A"
+        | 3 -> Day03.run () |> sprintf "Tree count: %s"
+        | 4 -> 
+            let passports,validCount,invalidCount = Day04.run ()
+            let testResult = Day04Tests.test ()
+            let newline = Environment.NewLine
+            sprintf "Passports: %A%s// Valid: %i , Invalid: %i %s// Test valid/invalid OK: %A" 
+                    passports newline validCount invalidCount newline testResult
+        | _ -> "unknown day"
+
     [<EntryPoint>]
     let main argv =
         try
             let cliArgs = CommandLine.parse argv
-            let result = 
-                match cliArgs.GetResult Day with 
-                | 1 -> Day01.run () |> sprintf "Product of 2020: %s"
-                | 2 -> Day02.run () |> sprintf "Valid pwds: %A"
-                | 3 -> Day03.run () |> sprintf "Tree count: %s"
-                | 4 -> 
-                    let passports,validCount,invalidCount = Day04.run ()
-                    let testResult = Day04Tests.test ()
-                    let newline = Environment.NewLine
-                    sprintf "Passports: %A%s// Valid: %i , Invalid: %i %s// Test valid/invalid OK: %A" 
-                            passports newline validCount invalidCount newline testResult
-                | _ -> "unknown day"
+            let day = cliArgs.GetResult Day
+            let result = run day
             printfn "%s" result
         with e ->
             printfn "%s" e.Message
