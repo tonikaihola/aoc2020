5b5f288bbd9bc74b6a2cf4edc68055aee91c9f61
Toni Kaihola <tonikaihola tonikaihola@users.noreply.github.com>
2020-12-06 17:55:04 +0200

    Day 1 part 2

--- Day01.fs
+++ Day01.fs
@@ -13,11 +13,23 @@ module Day01 =
         then Some (number, inverse)
         else None
 
+    let find2020_3 (numbers:int array) =
+        numbers 
+        |> Array.allPairs numbers 
+        |> Array.allPairs numbers 
+        |> Array.tryFind (fun (x, (y, z)) -> x + y + z = 2020)
+
     let run () =
         let lines = System.IO.File.ReadAllLines $"{__SOURCE_DIRECTORY__}/input/day01.txt"
         let numbers = lines |> Array.map Int32.Parse 
         let result = numbers |> Array.tryPick (find2020 numbers)
-        match result with
-        | Some (n,m) -> n * m
-        | None -> 0
-        |> string
+        let result2 = find2020_3 numbers
+        let s1 = match result with
+                 | Some (n,m) -> n * m
+                 | None -> 0
+                 |> string
+        let s2 = match result2 with
+                 | Some (x, (y, z)) -> x * y * z
+                 | None -> 0
+                 |> string
+        string (s1,s2)
